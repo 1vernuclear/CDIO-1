6736d21a192a1655e215ee0a8e2d8856415ec80b
Tobias Mayn <tobiasmayn@gmail.com>
2021-09-27 14:52:49 +0200

    Færdig Die klasse

--- src/Spil/Die.java
+++ src/Spil/Die.java
@@ -1,24 +1,23 @@
+package Spil;
 import java.lang.Math;
 
 public class Die {
     private int faceValue, numberOfSides;
 
     public Die (int sides){
-        faceValue = 1;
         numberOfSides = sides;
     }
 
     public Die {
-        faceValue = 1;
         numberOfSides = 6;
     }
 
     public int roll {
-        faceValue =
+        faceValue = (int)((Math.random()*numberOfSides)+1);
     }
 
-    public void setFaceValue {
-
+    public void setFaceValue(int value) {
+        faceValue = value;
     }
 
     public int getFaceValue {
