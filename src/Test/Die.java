766260d46e837e4a441e0a2f2565a7bd7b2405c1
Tobias Mayn <tobiasmayn@gmail.com>
2021-09-27 13:28:25 +0200

    Udkast af Die klassen

---
+++ src/Test/Die.java
@@ -0,0 +1,20 @@
+import java.lang.Math;
+
+public class Die {
+    int faceValue;
+    public Die{
+
+    }
+
+    public int roll{
+
+    }
+
+    public void setFaceValue{
+
+    }
+
+    public int getFaceValue{
+        return faceValue;
+    }
+}
