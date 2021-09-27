ad354ee4bb7d434dbcc3f8275042ec6c10bf3ae9
Tobias Mayn <tobiasmayn@gmail.com>
2021-09-27 15:09:32 +0200

    Tilføjet kommentarer til metoderne i Die klassen

--- src/Spil/Die.java
+++ src/Spil/Die.java
@@ -12,14 +12,24 @@ public class Die {
         numberOfSides = 6;
     }
 
+    /*
+    Metoden ruller terningen og returnerer et hel tal,
+    der benyttes som terningens øjne
+    */
     public int roll {
         faceValue = (int)((Math.random()*numberOfSides)+1);
     }
 
+    /*
+    Metode til at sætte en specifik værdi for terningens øjne
+    */
     public void setFaceValue(int value) {
         faceValue = value;
     }
 
+    /*
+    Metode til at få værdien af terningens øjne
+     */
     public int getFaceValue {
         return faceValue;
     }
