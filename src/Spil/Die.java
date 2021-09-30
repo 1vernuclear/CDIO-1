d31e751bef5125009d601a9c170acd93f85ee763
ChristianDonnerborg <S215809@dtu.dk>
2021-09-30 22:37:14 +0200

    Kommentarer til Die

--- src/Spil/Die.java
+++ src/Spil/Die.java
@@ -11,20 +11,31 @@ public class Die {
     public Die (int sides){
         numberOfSides = sides;
     }
-
+    /**
+     * Constructor sets number of sides to 6
+     */
     public Die() {
         numberOfSides = 6;
     }
-
+    /**
+     * Rolls die and returns integer
+     */
     public void rollDie() {
         faceValue = random.nextInt(numberOfSides)+1;
     }
-
+    /**
+     * Sets specific value of eyes on die
+     * @param value: int, sets new value of faceValue.
+     */
     public void setFaceValue(int value) {
         faceValue = value;
     }
-
+    /**
+     *  Return face value of die
+     */
     public int getFaceValue(){
         return faceValue;
+
     }
+
 }
